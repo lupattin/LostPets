235bdbb61ab0885d25adffecf0706527c8e50ae5
Luca <lupattin@gmail.comgit config --global user.email lupattin@gmail.comgit config --global user.email lupattin@gmail.com>
2022-05-19 15:29:07 -0300

    prueba de env

--- server/lib/sendgrid.ts
+++ server/lib/sendgrid.ts
@@ -1,5 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+console.log(process.env.SENDGRID_API_KEY);
 
 export async function sendMail(to, petname, username, phone, where){
     
